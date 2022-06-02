7e665d79137882614efd3ccad69c47c08ad22d5b
zarembw1 <96263342+zarembw1@users.noreply.github.com>
2022-06-02 23:24:25 +0200

    Update GraphSaver.java
    poprawki w saverze

--- src/GraphSaver.java
+++ src/GraphSaver.java
@@ -1,4 +1,4 @@
-package pl.jimp.pathfinder;
+package com.example.graphi;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -6,60 +6,43 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class GraphSaver {
-    Graph graph;
-    String outputFileName;
+    private IGraph graph;
+    private File file;
 
-    public GraphSaver(Graph graph, String outputFileName) {
+    public GraphSaver(IGraph graph, File file) {
         this.graph = graph;
-        this.outputFileName = outputFileName;
+        this.file = file;
     }
 
-    public InfoLabel saveGraph() {
-        if(graph == null){
-            return new InfoLabel("Nie zaladowano grafu.", InfoLabelSource.SAVE, true);
-        }
-        String outputFilePath = "src/main/resources/pl/jimp/pathfinder/data/" + outputFileName; // sciezka do zmienienia
-        File outputFile = new File(outputFilePath);
-
-        if (!outputFile.exists()) {
+    public boolean saveGraph() {
+        if (graph == null)
+            return false;
+        INode node;
             try {
-                outputFile.createNewFile();
-            } catch (IOException e) {
-                return new InfoLabel("NIe mozna stworzyc pliku '" + outputFileName +"'.", InfoLabelSource.SAVE, true);
-            }
-        }
+                FileWriter fileWriter = new FileWriter(this.file, false);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
 
-
-        try {
-            FileWriter fileWriter = new FileWriter(outputFilePath, false);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-
-            printWriter.println(graph.getHeight() + " " + graph.getWidth());
-            for (int vertexIndex = 0; vertexIndex < graph.getHeight() * graph.getWidth(); vertexIndex++) {
-                printWriter.print("\t");
-                for (int weightIndex = 0; weightIndex < 4; weightIndex++) {
-                    if (graph.getNodeNumber().get(vertexIndex).getWeights().get(weightIndex) != 0) {
-                        switch (weightIndex) {
-                            case 0 -> printWriter.print((vertexIndex - graph.getWidth()) + " :" + graph.getNodeNumber().get(vertexIndex).getWeight().get(0));
-                            case 1 -> printWriter.print((vertexIndex + graph.getWidth()) + " :" + graph.getNodeNumber().get(vertexIndex).getWeight().get(1));
-                            case 2 -> printWriter.print((vertexIndex + 1) + " :" + graph.getNodeNumber().get(vertexIndex).getWeight().get(2));
-                            case 3 -> printWriter.print((vertexIndex - 1) + " :" + graph.getNodeNumber().get(vertexIndex).getWeight().get(3));
+                printWriter.println("\t" + graph.getHeight() + " " + graph.getWidth());
+                for (int vertexIndex = 0; vertexIndex < graph.getHeight() * graph.getWidth(); vertexIndex++) {
+                    printWriter.print("\t");
+                    node = graph.getNode(vertexIndex);
+                    for (int weightIndex = 0; weightIndex < node.getConnectionsAmount(); weightIndex++) {
+                        if (node.getConnectionWeight(weightIndex) != -1) {
+                            printWriter.print(node.getConnectedNode(weightIndex) + " :" + node.getConnectionWeight(weightIndex));
+                            printWriter.print("\t");
                         }
-                        printWriter.print(" ");
                     }
+                    printWriter.print("\n");
                 }
-                printWriter.print("\n");
+
+
+                printWriter.close();
+                fileWriter.close();
+
+            } catch (IOException e) {
+                return false;
             }
-
-
-            printWriter.close();
-            fileWriter.close();
-
-        } catch (IOException e) {
-            return new InfoLabel("Nie mozna zapisac pliku '" + outputFileName + "'.", InfoLabelSource.SAVE, true);
-        }
-
-
-        return new InfoLabel("Graf zapisany do '" + outputFileName + "'.", InfoLabelSource.SAVE, false);
+        return true;
     }
+
 }
